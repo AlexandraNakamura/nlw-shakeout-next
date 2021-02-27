998dcac7b44d5160e574a03767f3ea36a45067a8
Naka <desginernakamura@gmail.com>
2021-02-27 02:25:26 -0300

    Adding challengebox

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -1,9 +1,31 @@
 import styles from '../styles/components/ChallengeBox.module.css';
 
 export function ChallengeBox() {
+    const hasActiveChallenge = true;
+    
+
     return (
         <div className={styles.challengeBoxContainers}>
-            <div className={styles.challengeBoxNotActive}>
+            { hasActiveChallenge ? (
+                <div className={styles.challengeActive}>
+                    <header>Ganhe 400 xp </header>
+
+                    <main>
+                        <img src="icons/body.svg" />
+                        <strong> Novo desafio</strong>
+                        <p>Levante e faça uma caminhada de 3 minutos</p>
+                    </main>
+                    <footer>
+                        <button type="button" className={styles.challengeFailedButton}>
+                            Falhei
+                        </button>
+                        <button type="button" className={styles.challengeSucceededButton}>
+                            Completei
+                        </button>
+                    </footer>
+                    </div>
+            ) : (
+                <div className={styles.challengeBoxNotActive}>
                 <strong>
                     Finalize um ciclo para receber um desafio
                 </strong>
@@ -11,7 +33,8 @@ export function ChallengeBox() {
                     <img src="icons/level-up.svg" alt="Level Up" />
                     Avace de level completando desafios.
                 </p>
-            </div>
+            </div>  
+            )}
         </div>
     )
 }
